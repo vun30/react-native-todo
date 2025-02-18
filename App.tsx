ecafb6264ca631eb8598b7c73d6e669521a60c4f
vun30 <hoanvu3012@gmail.com>
2025-02-18 18:28:26 +0700

    view style text

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello world!</Text>
+      <Text style={{ color: "red", fontSize: 30 }}>An an loz!</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
