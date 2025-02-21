bfaea79e6bae84c1588cb0732d8946cf903a08ee
vun30 <hoanvu3012@gmail.com>
2025-02-22 00:11:15 +0700

    usestate

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
-
+import { useState } from "react";
 export default function App() {
+  // const [name, setName] = useState<String>("eric");
+  // const [test, setTest] = useState({
+  //   name:'vu',
+  //   age: 22
+  // })
+  const [count, setCount] = useState<number>(0)
   return (
     <View style={styles.container}>
-      <Text style={{ color: "red", fontSize: 30 }}>An an loz!</Text>
-      
+      <Text style={{ color: "red", fontSize: 30 }}>Count = {count}</Text>
+      <View>
+        <Button title="Increase" onPress={() => setCount(count + 1)}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
