21699260b847370edb73faf79ed1f0819e19a53a
vun30 <hoanvu3012@gmail.com>
2025-02-25 15:27:41 +0700

    text input

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import { useState } from "react";
 export default function App() {
   // const [name, setName] = useState<String>("eric");
@@ -7,12 +7,43 @@ export default function App() {
   //   name:'vu',
   //   age: 22
   // })
-  const [count, setCount] = useState<number>(0)
+  const [count, setCount] = useState<number>(0);
+  const [name, setName] = useState<String>("")
+  const [age,setAge] = useState<number>(0)
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={{ fontSize: 30 }}>Name:{name}</Text>
+        <TextInput
+          multiline
+          autoCapitalize={"words"}
+          onChangeText={(value) => setName(value)}
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15,
+          }}
+        ></TextInput>
+      </View>
+      <View>
+        <Text style={{ fontSize: 30 }}>Age:{age}</Text>
+        <TextInput
+          onChangeText={(value) => setAge(+value)}
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15,
+            
+          }}
+          keyboardType="numeric"
+          maxLength={2}
+        ></TextInput>
+      </View>
       <Text style={{ color: "red", fontSize: 30 }}>Count = {count}</Text>
       <View>
-        <Button title="Increase" onPress={() => setCount(count + 1)}/>
+        <Button title="Increase" onPress={() => setCount(count + 1)} />
       </View>
       <StatusBar style="auto" />
     </View>
